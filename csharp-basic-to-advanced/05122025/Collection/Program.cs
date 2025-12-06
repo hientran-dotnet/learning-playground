7fcf45af0c8d07ab0f3aa30f7077d573a6555f7a
HienTran <hientm0978@gmail.com>
2025-12-06 13:42:10 +0700

    Collection - Generic & No Generic

---
+++ csharp-basic-to-advanced/05122025/Collection/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+
+namespace Collection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Collection - List<T>
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6};
+
+            Console.WriteLine(string.Join(", ", numbers));
+
+            // Dictionary<TKey, TValue>
+            Dictionary<string, int> studentScores = new Dictionary<string, int>()
+            {
+                {"Alice", 90},
+                {"Bob", 85},
+                {"Charlie", 92  }
+            };
+            Console.WriteLine($"Student Scores: {studentScores["Alice"]}");
+
+            // ArrayList
+            ArrayList list = new ArrayList() { 1, "hello", true };
+            list.Add(3.14);
+            Console.WriteLine(string.Join(", ", list.Cast<object>()));
+
+            // Hashtable
+            Hashtable hashtable = new Hashtable()
+            {
+                {"Alice", 80},
+                {"Bob", 75},
+                {"Charlie", 88}
+            };
+            Console.WriteLine($"Hashtable - Bob's score: {hashtable["Bob"]}");
+        }
+    }
+}
---
+++ csharp-basic-to-advanced/05122025/CollectionPractices/Program.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Diagnostics;
+
+namespace CollectionPractices
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Bài 1:
+            List<string> students = new List<string>() { "Alice", "Bob", "Mary", "Peter", "Jack", "Lamvie" };
+            students.Add("David");
+            Console.WriteLine("List students after add: ");
+            Console.WriteLine(string.Join(", ", students));
+
+            // Xóa theo tên
+            students.Remove("Bob");
+            Console.WriteLine("List students after remove \"Bob\"");
+            Console.WriteLine(string.Join(", ", students));
+
+            // Xóa theo index
+            students.RemoveAt(2);
+            Console.WriteLine("List students after remove value at index 2");
+            Console.WriteLine(string.Join(", ", students));
+
+            // Search tên trong list
+            string searchName = "Jack";
+            if (students.Contains(searchName))
+            {
+                Console.WriteLine($"{searchName} is exits");
+            }
+            else
+            {
+                Console.WriteLine($"{searchName} is not exits");
+            }
+
+            Console.WriteLine("\n\n\n\n\n");
+
+
+            // Bài 2:
+            Dictionary<string, int> studentScores = new Dictionary<string, int>()
+            {
+                { "Hien", 10 },
+                { "Huy", 0 },
+                { "Hung", 7 },
+                { "Phuong", 9 }
+            };
+
+            Console.WriteLine("List students have mark greater than 8: ");
+            foreach (var student in studentScores)
+            {
+                if(student.Value > 8)
+                {
+                    Console.WriteLine($"{student.Key}: {student.Value}");
+                }
+            }
+
+            // Bài 3:
+            const int itemCount = 1000000;
+            
+            ArrayList arrayList = new ArrayList();
+
+            for(int i = 0; i < itemCount; i++)
+            {
+                arrayList.Add(i);
+            }
+
+            List<int> list = new List<int>();
+
+            for (int i = 0; i < itemCount; i++)
+            {
+                list.Add(i);
+            }
+
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
+            foreach (int item in arrayList)
+            {
+                int temp = item;
+            }
+            stopwatch.Stop();
+            Console.WriteLine($"Time spend: {stopwatch.ElapsedMilliseconds}");
+
+            stopwatch.Restart();
+
+            foreach (int item in list)
+            {
+                int temp = item;
+            }
+            stopwatch.Stop();
+            Console.WriteLine($"Time spend: {stopwatch.ElapsedMilliseconds}");
+        }
+    }
+}
