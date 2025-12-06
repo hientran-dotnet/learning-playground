975372a522e50f7c23d113c5f8a1b2d555471e58
HienTran <hientm0978@gmail.com>
2025-12-06 17:42:09 +0700

    Collection practice with Queu & Stack

---
+++ csharp-basic-to-advanced/06122025/CollectionPratice/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace CollectionPratice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> customer = new Queue<string>();
+            while (true)
+            {
+                Console.WriteLine("\n1. Add new customer");
+                Console.WriteLine("2. Call the next customer");
+                Console.WriteLine("3. Show list customer");
+                Console.WriteLine("0. Exit");
+                Console.WriteLine("Please choose a function: ");
+                int choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Enter customer name: ");
+                        customer.Enqueue(Console.ReadLine());
+                        break;
+                    case 2:
+                        if (customer.Count > 0)
+                        {
+                            Console.WriteLine($"Call customer: {customer.Dequeue()}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Customer queue is empty");
+                        }
+                        break;
+                    case 3:
+                        if(customer.Count > 0)
+                        {
+                            Console.WriteLine("List customer in the queue: ");
+                            foreach (string item in customer)
+                            {
+                                Console.WriteLine(item);
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Customer queue is empty!");
+                        }
+                            break;
+                    case 0:
+                        return;
+                    default:
+                        Console.WriteLine("Invalid choice");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ csharp-basic-to-advanced/06122025/ReverseStringStack/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ReverseStringStack
+{
+    internal class Program
+    {
+        static string ReverseString(string value)
+        {
+            // h, e, l, l, o
+            Stack<char> chars = new Stack<char>();
+            foreach (char c in value)
+            {
+                chars.Push(c);
+            }
+
+            string reverse = string.Empty;
+            while (chars.Count > 0)
+            {
+                reverse += chars.Pop();
+            }
+            return reverse;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input string:");
+            string input = Console.ReadLine();
+            string result = ReverseString(input);
+            Console.WriteLine(result);
+        }
+    }
+}
